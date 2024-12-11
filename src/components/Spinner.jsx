c91930d91e562e414c198b22b4fb34f6108571f4
ichami630 <brandonichami@gmail.com>
2024-12-11 17:47:31 +0000

    added spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import RingLoader from 'react-spinners/RingLoader'
+
+
+const override = {
+    display: 'block',
+    margin: '100px auto',
+}
+
+const Spinner = ( { loading } ) => {
+  return (
+    <RingLoader
+        color='#4338ca'
+        loading={loading}
+        cssOverride={override}
+        size={150}
+    />
+  )
+}
+
+export default Spinner
