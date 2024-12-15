482a7da8fd977fb29bb731081d9d776d8f3ca147
ichami630 <brandonichami@gmail.com>
2024-12-15 10:07:47 +0000

    added add job functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import { Route, createBrowserRouter, createRoutesFromElements,RouterProvider,} f
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import JobPage,{jobLoader} from './components/JobPage';
+import AddJobPage from './pages/AddJobPage';
 import NotFound from './pages/NotFound';
 
+const addjob = async (newJob) => {
+  const res = await fetch("/api/jobs",{
+    method: 'POST',
+    headers: {
+      'content-Type': 'application/json',
+    },
+    body: JSON.stringify(newJob),
+  });
+  return;
+}
 const router = createBrowserRouter(
   createRoutesFromElements(
     // parent route (MainLayout)
@@ -12,6 +24,8 @@ const router = createBrowserRouter(
       {/* child route e.g HomePage,JobsPage */}
       <Route index element={ <HomePage /> }/>
       <Route path='/jobs' element={ <JobsPage /> }/>
+      <Route path='/jobs/:id' element={ <JobPage /> } loader={jobLoader}/>
+      <Route path='/add-job' element={ <AddJobPage addJobSubmit={addjob} /> }/>
       <Route path='*' element={ <NotFound /> }/>
     </Route>
   )
